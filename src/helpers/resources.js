e0e2ce108f9ce796136542082ca31db6a5ee0109
Aleks <lasacc4@yandex.ru>
2022-11-21 15:26:35 +0300

    fixed locales

--- src/helpers/resources.js
+++ src/helpers/resources.js
@@ -2,7 +2,7 @@ export default {
     ru: {
         translation: {
             'Enter valid url': 'Ссылка должна быть валидным URL',
-            'Url must be unique': 'RSS уже существует,',
+            'Url must be unique': 'RSS уже существует',
             'Enter your RSS link': 'Не должно быть пустым',
             'RSS success': 'RSS успешно загружен',
             'posts': 'Посты',
